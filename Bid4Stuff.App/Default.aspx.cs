22fdcf1c3c540381d92f78185de2b8cf22acb973
Kiril Tsarnakliyski <elinos@gmail.com>
2014-10-20 17:47:14 +0300

    Home Page

--- Bid4Stuff.App/Default.aspx.cs
+++ Bid4Stuff.App/Default.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bid4Stuff.Data;
+using Bid4Stuff.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,23 @@ namespace Bid4Stuff.App
 {
     public partial class _Default : Page
     {
+        private const int TopItemsCount = 15;
+        private Bid4StuffData db = new Bid4StuffData();
+        
         protected void Page_Load(object sender, EventArgs e)
         {
+        }
 
+        public IQueryable<Item> ListViewLatestAddedOffers_GetData()
+        {
+            var latestAddedItems = this.db.Items.All().OrderByDescending(i => i.StartDate).Take(TopItemsCount);
+            return latestAddedItems;
+        }
+
+        public IQueryable<Item> ListViewOffersEndingSoon_GetData()
+        {
+            var latestAddedItems = this.db.Items.All().OrderBy(i => i.EndDate).Take(TopItemsCount);
+            return latestAddedItems;
         }
     }
 }
