fa9b66b4687a18891c8454d9d3dbf80e788b50d9
Kiril Tsarnakliyski <elinos@gmail.com>
2014-10-22 17:43:57 +0300

    Temp set of CategoryID to be nullable

--- Bid4Stuff.Models/Item.cs
+++ Bid4Stuff.Models/Item.cs
@@ -16,7 +16,7 @@
 
         public string Description { get; set; }
 
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
 
         public virtual Category Category { get; set; }
 
